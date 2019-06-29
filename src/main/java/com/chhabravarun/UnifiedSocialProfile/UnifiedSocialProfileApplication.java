170fa8fd305018bac92f67dfc473597277250cd5
varunchhabra16 <varunchhabra.16@gmail.com>
2019-06-29 10:39:30 +0530

    created controller class skeleton

--- src/main/java/com/chhabravarun/UnifiedSocialProfile/UnifiedSocialProfileApplication.java
+++ src/main/java/com/chhabravarun/UnifiedSocialProfile/UnifiedSocialProfileApplication.java
@@ -2,12 +2,20 @@ package com.chhabravarun.UnifiedSocialProfile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UnifiedSocialProfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnifiedSocialProfileApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
---
+++ src/main/java/com/chhabravarun/UnifiedSocialProfile/controller/CFUserController.java
@@ -0,0 +1,17 @@
+package com.chhabravarun.UnifiedSocialProfile.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class CFUserController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+}
