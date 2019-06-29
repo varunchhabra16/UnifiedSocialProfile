e6d2e77d74102bb87f5cd8648a4ea01c10817421
Prabhat Singh Tomar <iampstomar@gmail.com>
2019-06-29 11:11:34 +0530

    Updated User model

--- src/main/java/com/chhabravarun/UnifiedSocialProfile/model/CFUser.java
+++ src/main/java/com/chhabravarun/UnifiedSocialProfile/model/CFUser.java
@@ -1,4 +1,38 @@
 package com.chhabravarun.UnifiedSocialProfile.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class CFUser {
+
+    @Getter @Setter
+    private String lastName;
+    @Getter @Setter
+    private String country;
+    @Getter @Setter
+    private Integer rating;
+    @Getter @Setter
+    private String handle;
+    @Getter @Setter
+    private String firstName;
+    @Getter @Setter
+    private Integer contribution;
+    @Getter @Setter
+    private String organisation;
+    @Getter @Setter
+    private Integer rank;
+
+    @Override
+    public String toString() {
+        return "CFUser{" +
+                "lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", rating=" + rating +
+                ", handle='" + handle + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", contribution=" + contribution +
+                ", organisation='" + organisation + '\'' +
+                ", rank=" + rank +
+                '}';
+    }
 }
--- src/main/java/com/chhabravarun/UnifiedSocialProfile/model/Quote.java
+++ src/main/java/com/chhabravarun/UnifiedSocialProfile/model/Quote.java
@@ -1,4 +1,37 @@
 package com.chhabravarun.UnifiedSocialProfile.model;
 
+import com.chhabravarun.UnifiedSocialProfile.model.CFUser;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
+
+    private String status;
+    private CFUser result;
+
+    public Quote(){}
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public CFUser getResult() {
+        return result;
+    }
+
+    public void setResult(CFUser result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "status='" + status + '\'' +
+                ", result=" + result +
+                '}';
+    }
 }
